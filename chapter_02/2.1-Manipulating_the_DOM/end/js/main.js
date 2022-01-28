9d60d03d3c938d504aae6d1c536dcb19f49c20fb
Anne-Marie Dufour <hello@annemariedufour.com>
2022-01-28 15:34:59 +0100

    Chapter 2: Update end files

---
+++ chapter_02/2.1-Manipulating_the_DOM/end/js/main.js
@@ -0,0 +1,13 @@
+const svg = d3.select(".responsive-svg-container")
+  .append("svg")
+  .attr("viewBox", "0 0 1200 1600")
+  .style("border", "1px solid black");
+
+svg
+  .append("rect")
+  .attr("x", 10)
+  .attr("y", 10)
+  .attr("width", 414)
+  .attr("height", 16)
+  .attr("fill", "turquoise")
+  .style("fill", "plum");
