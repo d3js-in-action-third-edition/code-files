51e44eb97b6a6e785ea657143f6b85d7c2eab20e
Anne-Marie Dufour <hello@annemariedufour.com>
2022-01-31 17:53:14 +0100

    Chapter 3: Add scales section

--- chapter_03/3.4-Scales/end/js/main.js
+++ chapter_03/3.4-Scales/end/js/main.js
@@ -1,7 +1,7 @@
 // Append a SVG container
 const svg = d3.select(".responsive-svg-container")
   .append("svg")
-  .attr("viewBox", "0 0 1200 1600")
+  .attr("viewBox", "0 0 600 700")
   .style("border", "1px solid black");
 
 // Load, format and measure the dataset
@@ -33,20 +33,28 @@ d3.csv("../data/data.csv", d => {
 // Create the bar graph
 const createViz = (data) => {
 
+  // Declare scales
+  const xScale = d3.scaleLinear()
+    .domain([0, 1078])
+    .range([0, 600]);
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.technology))
+    .range([0, 1000])
+    .paddingInner(0.2);
+
   // Use data-binding to append rectangles
-  const barHeight = 20;
   svg
     .selectAll("rect")
     .data(data)
     .join("rect")
       .attr("class", d => {
-        console.log(d);
+        // console.log(d);
         return "bar";
       })
-      .attr("width", d => d.count)
-      .attr("height", barHeight)
+      .attr("width", d => xScale(d.count))
+      .attr("height", yScale.bandwidth())
       .attr("x", 0)
-      .attr("y", (d, i) => (barHeight + 5) * i)
+      .attr("y", d => yScale(d.technology))
       .attr("fill", d => d.technology === "D3.js" ? "yellowgreen" : "skyblue");
 
 };
