b19eb3284d3dc6c29583078a7253d0455f3eb677
Anne-Marie Dufour <hello@annemariedufour.com>
2022-02-22 09:26:17 +0100

    Chapter 4: Project design

--- chapter_04/end/js/arcs.js
+++ chapter_04/end/js/arcs.js
@@ -1,49 +1,28 @@
 const drawArcs = (data, regimes) => {
-  const data2000 = data.find(d => d.year === 2000);
   const data2020 = data.find(d => d.year === 2020);
-  console.log(data2000, data2020);
+  console.log(data2020);
 
   const data2020Formatted = [
     {regime:"liberal_democracies", numPeople:1066163865},
     {regime:"electoral_democracies", numPeople:2247950115},
     {regime:"electoral_autocracies", numPeople:992281450},
     {regime:"closed_autocracies", numPeople:1823737580},
-    {regime:"no_regime_data", numPeople:13643611},
-  ]
-  const data2000Formatted = [
-    {regime:"liberal_democracies", numPeople:1067360224},
-    {regime:"electoral_democracies", numPeople:1435304399},
-    {regime:"electoral_autocracies", numPeople:3366054285},
-    {regime:"closed_autocracies", numPeople:1909939094},
-    {regime:"no_regime_data", numPeople:16140723},
-  ]
+    // {regime:"no_regime_data", numPeople:13643611},
+  ];
 
   // Color scale
   colorScale = d3.scaleOrdinal()
     .domain(regimes)
     .range(colors);
 
-  const svg1 = d3.select("#pie-chart-1")
+  const svg = d3.select("#pie-chart")
     .append("svg")
-      .attr("viewBox", [0, 0, width/2, height/2])
-      .style("border", "1px solid black");
-  const svg2 = d3.select("#pie-chart-2")
-    .append("svg")
-      .attr("viewBox", [0, 0, width/2, height/2])
-      .style("border", "1px solid black");
+      .attr("viewBox", [0, 0, width/2, height/2]);
 
   // Append text in the middle
-  svg1
+  svg
     .append("text")
       .text("2000")
-      .attr("text-anchor", "middle")
-      .attr("alignment-baseline", "middle")
-      .attr("x", width/4)
-      .attr("y", height/4)
-      .style("font-size", "20px");
-  svg2
-    .append("text")
-      .text("2020")
       .attr("text-anchor", "middle")
       .attr("alignment-baseline", "middle")
       .attr("x", width/4)
@@ -52,12 +31,9 @@ const drawArcs = (data, regimes) => {
 
   // Initialize the path generator
   // need to generate array with values or array of objects with key-value pair
-  const arcs1 = d3.pie()
-    .value(d => d.numPeople)(data2000Formatted);
-  console.log("arcs1", arcs1)
-  const arcs2 = d3.pie()
+  const arcs = d3.pie()
     .value(d => d.numPeople)(data2020Formatted);
-  console.log("arcs2", arcs2)
+  console.log("arcs", arcs);
 
   // Initialize arc generator
   const arcGenerator = d3.arc()
@@ -68,25 +44,11 @@ const drawArcs = (data, regimes) => {
   
   // Append arcs
   // Could I control the order?
-  svg1
+  svg
     .append("g")
       .attr("transform", `translate(${width/4}, ${height/4})`)
     .selectAll("path.path-2020")
-    .data(arcs1)
-    .join("path")
-      .attr("class", "path-2020")
-      .attr("d", d => {
-        return arcGenerator({
-          startAngle: d.startAngle,
-          endAngle: d.endAngle
-        });
-      })
-      .attr("fill", d => colorScale(d.data.regime));
-  svg2
-    .append("g")
-      .attr("transform", `translate(${width/4}, ${height/4})`)
-    .selectAll("path.path-2020")
-    .data(arcs2)
+    .data(arcs)
     .join("path")
       .attr("class", "path-2020")
       .attr("d", d => {
