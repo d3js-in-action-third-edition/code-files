2a6690952fd554b583257dac4c947a09984481a4
Anne-Marie Dufour <hello@annemariedufour.com>
2022-03-09 08:58:17 +0100

    Add code for section 4.2

--- chapter_04/4.2-Margin_convention_and_axes/end/js/line-chart.js
+++ chapter_04/4.2-Margin_convention_and_axes/end/js/line-chart.js
@@ -1,9 +1,67 @@
 // Create the line chart here
-const drawLineChart = (data) => {
+const drawLineChart = (partialData) => {
 
+  /*******************************/
+  /*    Append the containers    */
+  /*******************************/
   // Append the SVG container
-  // const svg = d3.select("#line-chart")
-  //   .append("svg")
-  //     .attr("viewBox", [0, 0, width, height]) // How cool, viewbox can also be passed as an array!
-  //     .style("border", "1px solid black");
+  const svg = d3.select("#line-chart")
+    .append("svg")
+      .attr("viewBox", `0, 0, ${width}, ${height}`)
+      .style("border", "1px solid skyblue");
+
+  // Append the group that will contain the chart
+  const innerChart = svg
+    .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  
+  /****************************/
+  /*    Declare the scales    */
+  /****************************/
+  // X scale
+  const xScale = d3.scaleLinear()
+    .domain(d3.extent(partialData, d => d.year))
+    .range([0, innerWidth]);
+
+  // Y scale
+  const maxPeople = d3.max(partialData, d => {
+    const numPeoplePerRegime = [];
+    regimesInfo.forEach(regime => {
+      numPeoplePerRegime.push(d[regime.id]);
+    });
+    return Math.max(...numPeoplePerRegime);
+  });
+  
+  const yScale = d3.scaleLinear()
+    .domain([0, maxPeople])
+    .range([innerHeight, 0])
+    .nice();
+
+  
+  /***************************/
+  /*     Append the axes     */
+  /***************************/
+  // Bottom axis
+  const bottomAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.format("d"))
+    .tickSize(innerHeight * -1);
+  const axis = innerChart
+    .append("g")
+      .attr("class", "axis-x")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(bottomAxis);
+  d3.selectAll(".axis-x text")
+    .attr("y", "10px");
+
+  // Left axis
+  const leftAxis = d3.axisLeft(yScale)
+    .tickFormat(d3.format("~s"))
+    .tickSize(innerWidth * -1);
+  innerChart
+    .append("g")
+      .attr("class", "axis-y")
+      .call(leftAxis);
+  d3.selectAll(".axis-y text")
+    .attr("dx", "-5px");
 };
