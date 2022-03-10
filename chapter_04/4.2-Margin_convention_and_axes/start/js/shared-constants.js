8302cf8f705f8ee8c43a3d6885a0d00627b54dfe
Anne-Marie Dufour <hello@annemariedufour.com>
2022-03-10 12:47:43 +0100

    Add line chart series section

--- chapter_04/4.2-Margin_convention_and_axes/start/js/shared-constants.js
+++ chapter_04/4.2-Margin_convention_and_axes/start/js/shared-constants.js
@@ -1,9 +1,10 @@
 const regimesInfo = [
-  {id: "liberal_democracies", label: "Liberal democracies", color: "#26456e"},
-  {id: "electoral_democracies", label: "Electoral democracies", color: "#5aa5cd"},
-  {id: "electoral_autocracies", label: "Electoral autocracies", color: "#f98c40"},
-  {id: "closed_autocracies", label: "Closed autocracies", color: "#d04a06"},
-  {id: "no_regime_data", label: "No regime data", color: "#555555"},
+  {id: "liberal_democracies", label: "Liberal democracies"},
+  {id: "electoral_democracies", label: "Electoral democracies"},
+  {id: "electoral_autocracies", label: "Electoral autocracies"},
+  {id: "closed_autocracies", label: "Closed autocracies"},
+  {id: "no_regime_data", label: "No regime data"},
 ];
+const colors = ["#26456e", "#5aa5cd", "#f98c40", "#d04a06", "#555555"];
 
 // Add other shared contants here
