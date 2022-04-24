96a80c2fe37a47a0d7957ac0e1e596eae0c2feb3
Anne-Marie Dufour <hello@annemariedufour.com>
2022-04-24 13:50:04 +0200

    Chapter 04: Add skeleton files for section 4.2

---
+++ chapter_04/4.2-Drawing_a_line_chart/end/js/line-chart.js
@@ -0,0 +1,91 @@
+// Load the data here
+d3.csv("../data/weekly_temperature.csv", d3.autoType).then(data => {
+  console.log("temperature data", data);
+  drawLineChart(data);
+});
+
+// Create the line chart here
+const drawLineChart = (data) => {
+
+  /*******************************/
+  /*    Declare the constants    */
+  /*******************************/
+  const margin = {top: 40, right: 170, bottom: 25, left: 40};
+  const width = 1000;
+  const height = 500;
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+
+  /*******************************/
+  /*    Append the containers    */
+  /*******************************/
+  // Append the SVG container
+  const svg = d3.select("#line-chart")
+    .append("svg")
+      .attr("viewBox", `0, 0, ${width}, ${height}`);
+
+  // Append the group that will contain the inner chart
+  const innerChart = svg
+    .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+  
+  /****************************/
+  /*    Declare the scales    */
+  /****************************/
+  // X scale
+  const firstDate = d3.min(data, d => d.date);
+  const lastDate = d3.max(data, d => d.date);
+  const xScale = d3.scaleTime()
+    .domain([d3.timeMonth.floor(firstDate), lastDate])
+    .range([0, innerWidth]);
+
+  // Y scale
+  const maxTemp = d3.max(data, d => d.max_temp_F);
+  const yScale = d3.scaleLinear()
+    .domain([0, maxTemp])
+    .range([innerHeight, 0]);
+
+  
+  /***************************/
+  /*     Append the axes     */
+  /***************************/
+  // Bottom axis
+  const bottomAxis = d3.axisBottom(xScale)
+    .tickFormat(d3.timeFormat("%b"));
+  innerChart
+    .append("g")
+      .attr("class", "axis-x")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(bottomAxis);
+  d3.selectAll(".axis-x text")
+    .attr("x", d => {
+       const currentMonth = d;
+       const nextMonth = new Date(2021, currentMonth.getMonth() + 1, 1);
+       return (xScale(nextMonth) - xScale(currentMonth)) / 2;
+    })
+    .attr("y", "10px");
+
+  // Left axis
+  const leftAxis = d3.axisLeft(yScale);
+  innerChart
+    .append("g")
+      .attr("class", "axis-y")
+      .call(leftAxis);
+  d3.selectAll(".axis-y text")
+    .attr("dx", "-5px");
+
+  // Set the font-family and font-size property of axis labels
+  // This could also be handled from a CSS file
+  d3.selectAll(".axis-x text, .axis-y text")
+    .style("font-family", "Roboto, sans-serif")
+    .style("font-size", "14px");
+
+  // Add label to the y-axis
+  svg
+    .append("text")
+      .text("Temperature (°F)")
+      .attr("y", 20);
+
+};
