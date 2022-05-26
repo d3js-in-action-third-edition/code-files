8af37671bd3c1f9cd1cffbbd31180eef92dad2ad
Anne-Marie Dufour <hello@annemariedufour.com>
2022-05-26 12:36:26 +0200

    Chapter 5: Update path to data file

--- chapter_05/5.1-Pie_layout/end/js/load-data.js
+++ chapter_05/5.1-Pie_layout/end/js/load-data.js
@@ -1,5 +1,5 @@
 // Load data
-d3.csv("./data/data.csv", d3.autoType).then(data => {
+d3.csv("../data/data.csv", d3.autoType).then(data => {
   defineScales(data);
   drawDonutCharts(data);
   drawStackedBars(data);
