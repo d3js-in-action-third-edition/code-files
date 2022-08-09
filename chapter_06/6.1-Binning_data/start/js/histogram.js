6bb46c3fc27494c1c2f9a911901d32d60c401159
Anne-Marie Dufour <hello@annemariedufour.com>
2022-08-09 11:56:21 +0200

    Chapter 6: Add data, js and css starting files

---
+++ chapter_06/6.1-Binning_data/start/js/histogram.js
@@ -0,0 +1,27 @@
+const drawHistogram = (data) => {
+
+  /*******************************/
+  /*    Declare the constants    */
+  /*******************************/
+  const margin = {top: 40, right: 30, bottom: 50, left: 40};
+  const width = 1000;
+  const height = 500;
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+
+  /*******************************/
+  /*    Append the containers    */
+  /*******************************/
+  // Append the SVG container
+  const svg = d3.select("#histogram")
+    .append("svg")
+      .attr("viewBox", `0, 0, ${width}, ${height}`);
+
+  // Append the group that will contain the inner chart
+  const innerChart = svg
+    .append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+
+};
