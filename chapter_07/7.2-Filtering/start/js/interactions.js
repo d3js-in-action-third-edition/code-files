a0dcfba4e718581a94f4e8c495b6aa3cf42741af
Anne-Marie Dufour <hello@annemariedufour.com>
2022-10-14 10:57:19 +0200

    Chapter 07: Add start folder

---
+++ chapter_07/7.2-Filtering/start/js/interactions.js
@@ -0,0 +1,18 @@
+/*************************************/
+/*  Create and populate the filters  */
+/*************************************/
+const populateFilters = (data) => {
+
+  // Create the filters here
+
+};
+
+
+/********************************/
+/*   Handle clicks on filters   */
+/********************************/
+const filterHistogram = (selectedOption, data) => {
+  
+  // Handle clicks on filters here
+
+};
---
+++ chapter_07/7.2-Filtering/start/js/load-data.js
@@ -0,0 +1,20 @@
+const getRandomSalary = (salary) => {
+  const lowerLimit = +salary.slice(1, salary.indexOf(" -")).replace(",", "");
+  const upperLimit = +salary.slice(salary.indexOf(" $") + 2).replace(",", "");
+  
+  return Math.floor(Math.random() * (upperLimit - lowerLimit) + lowerLimit);
+}
+
+// Load data
+d3.csv("./data/data.csv", d => {
+  if (d.pay_annual_USD !== "$240,000 or more") {
+    return {
+      role: d.role,
+      gender: d.gender,
+      salary: getRandomSalary(d.pay_annual_USD)
+    };
+  }
+}).then(data => {
+  drawHistogram(data);
+  populateFilters(data);
+});
