71d5b8ca5c4353790f1a07dd2a39e025d49ab501
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-05-24 18:50:59 +0200

    Chapter 14: Add paintings circles

---
+++ chapter_14/14.5.2-Force_layout/end/src/chart_components/Paintings.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let paintingAreaScale;
+  export let paintingDefaultRadius;
+  export let paintings;
+  export let monthScale;
+  export let radius;
+</script>
+
+{#each paintings as painting}
+  <circle
+    cx={painting.month !== ""
+      ? radius * Math.sin(monthScale(painting.month))
+      : 0}
+    cy={painting.month !== ""
+      ? -1 * radius * Math.cos(monthScale(painting.month))
+      : 0}
+    r={painting.area_cm2
+      ? Math.sqrt(paintingAreaScale(painting.area_cm2) / Math.PI)
+      : paintingDefaultRadius}
+  />
+{/each}
