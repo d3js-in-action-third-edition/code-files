519814665f4ef7c7faa48af77a5c4f5225ca36de
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-05-25 12:23:57 +0200

    Chapter 14: Add radial bar chart

---
+++ chapter_14/15.4.4-Radial_bar_chart/end/src/chart_components/Letters.svelte
@@ -0,0 +1,27 @@
+<script>
+  import { months } from "../utils/months";
+
+  export let letters;
+  export let monthScale;
+  export let radialScale;
+</script>
+
+{#each months as month}
+  <line
+    x1={0}
+    y1={0}
+    x2={radialScale(letters.find((l) => l.month === month).number_of_letters) *
+      Math.sin(monthScale(month))}
+    y2={radialScale(letters.find((l) => l.month === month).number_of_letters) *
+      Math.cos(monthScale(month))}
+    stroke-width={2}
+    stroke-linecap="round"
+  />
+{/each}
+
+<style lang="scss">
+  line {
+    stroke: $secondary;
+    pointer-events: none;
+  }
+</style>
---
+++ chapter_14/15.4.4-Radial_bar_chart/end/src/layout/Grid.svelte
@@ -0,0 +1,114 @@
+<script>
+  import GridItem from "./GridItem.svelte";
+  import { range, max } from "d3-array";
+  import { scaleLinear } from "d3-scale";
+  import paintings from "../data/paintings.json";
+  import drawings from "../data/drawings.json";
+  import { months } from "../utils/months";
+  import letters from "../data/letters.json";
+
+  let windowWidth;
+  const gridContainer = 1400;
+  const padding = 30;
+  let svgWidth;
+  $: switch (true) {
+    case windowWidth >= gridContainer:
+      svgWidth = (10 / 12) * (gridContainer - 2 * padding);
+      break;
+    case windowWidth < gridContainer && windowWidth >= 768:
+      svgWidth = (10 / 12) * (windowWidth - 2 * padding);
+      break;
+    default:
+      svgWidth = windowWidth - 2 * padding;
+  }
+
+  const years = range(1881, 1891);
+  let numColumns;
+  $: switch (true) {
+    case windowWidth > 900:
+      numColumns = 3;
+      break;
+    case windowWidth <= 900 && windowWidth > 600:
+      numColumns = 2;
+      break;
+    default:
+      numColumns = 1;
+  }
+  $: numRows = Math.ceil(years.length / numColumns);
+  $: smWidth = svgWidth / numColumns;
+  $: smHeight = smWidth + 40;
+  $: svgHeight = numRows * smHeight;
+
+  // Extract the areas of the paintings and find the max area
+  paintings.forEach((painting) => {
+    if (painting.width_cm && painting.height_cm) {
+      painting["area_cm2"] = painting.width_cm * painting.height_cm;
+    }
+  });
+  const maxPaintingArea = max(paintings, (d) => d.area_cm2);
+
+  // Set the scale for the area of the paintings
+  const maxPaintingRadius = 8;
+  const paintingDefaultRadius = 3; // For the paintings whose dimensions are unknown
+  const paintingAreaScale = scaleLinear()
+    .domain([0, maxPaintingArea])
+    .range([0, Math.PI * Math.pow(maxPaintingRadius, 2)]);
+
+  // Reorganize the drawings on a yearly basis
+  const yearlyDrawings = [];
+  years.forEach((year) => {
+    const relatedDrawings = { year: year, months: [] };
+    months.forEach((month) => {
+      relatedDrawings.months.push({
+        month: month,
+        drawings: drawings.filter(
+          (drawing) =>
+            drawing.year === year.toString() && drawing.month === month
+        ),
+      });
+    });
+    yearlyDrawings.push(relatedDrawings);
+  });
+
+  // Calculate the maximum number of drawings for a month
+  const maxDrawings = max(yearlyDrawings, (d) =>
+    max(d.months, (i) => i.drawings.length)
+  );
+</script>
+
+<svelte:window bind:innerWidth={windowWidth} />
+
+{#if svgWidth && svgHeight}
+  <svg width={svgWidth} height={svgHeight}>
+    {#each years as year, i}
+      <g
+        transform="translate(
+          {(i % numColumns) * smWidth},
+          {Math.floor(i / numColumns) * smHeight})"
+      >
+        <rect x={0} y={0} width={smWidth} height={smHeight} />
+        <GridItem
+          {smWidth}
+          {smHeight}
+          {year}
+          {paintingAreaScale}
+          {paintingDefaultRadius}
+          paintings={paintings.filter((painting) => painting.year === year)}
+          {maxDrawings}
+          drawings={yearlyDrawings.find((d) => d.year === year).months}
+          letters={letters.filter((letter) => letter.year === year)}
+        />
+      </g>
+    {/each}
+  </svg>
+{/if}
+
+<style>
+  svg {
+    border: 1px solid magenta;
+  }
+  rect {
+    fill: none;
+    stroke: cyan;
+  }
+</style>
