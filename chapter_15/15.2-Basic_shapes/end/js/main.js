b1f0e9e9a62ce746a241737f36ed3c7d2fc3e02b
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-06-12 15:26:12 +0200

    Chapter 15: Update SVG coordinates

--- chapter_15/15.2-Basic_shapes/end/js/main.js
+++ chapter_15/15.2-Basic_shapes/end/js/main.js
@@ -26,3 +26,52 @@ window.addEventListener("resize", setCanvasSize);
 // The context can be 2d or WebGL (3d).
 const context = canvas.node().getContext("2d");
 context.scale(devicePixelRatio, devicePixelRatio);
+
+// Drawing the line
+context.strokeStyle = "#000";
+context.lineWidth = 1;
+context.beginPath();
+context.moveTo(66.67, 60);
+context.lineTo(186.67, 300);
+context.stroke();
+
+// // Drawing the rectangles
+// context.fillStyle = "#6ba5d7";
+// context.fillRect(260, 25, 120, 60); // x, y, width, height
+
+// context.beginPath();
+// context.roundRect(260, 100, 120, 60, [20]);
+// context.fill();
+
+// context.fillStyle = "#fff";
+// context.fillRect(260, 175, 60, 60);
+// context.strokeStyle = "#6ba5d7";
+// context.strokeRect(260, 175, 60, 60);
+
+// // Drawing the circles
+// context.strokeStyle = "#81c21c";
+// context.lineWidth = 3;
+// context.beginPath();  // Try without it to see how another line is drawn
+// context.arc(530, 80, 50, 0,  2 * Math.PI, true);  // x, y, radius, startAngle, endAngle, isCounterClockwise
+// context.stroke();
+
+// context.fillStyle = "#81c21c";
+// context.beginPath();
+// context.ellipse(530, 205, 50, 30, 0, 0, 2 * Math.PI);
+// context.fill();
+
+// // Drawing the path
+// context.strokeStyle = "#773b9a";
+// const path = new Path2D("M680 150 C 710 80, 725 80, 755 150 S 810 220, 840 150");
+// context.stroke(path);
+
+// // Adding the text elements
+// context.fillStyle = "#636466";
+// context.font = "16px monospace";
+// context.fillText("line", 60, 260);
+// context.fillText("rect", 260, 260);
+// context.fillText("path", 730, 260);
+
+// context.textAlign = "center";
+// context.fillText("circle", 530, 155);
+// context.fillText("ellipse", 530, 260);
