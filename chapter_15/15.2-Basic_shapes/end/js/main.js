f6295dc322284fdaaeb86fba0e8df953ceed250b
Anne-Marie Dufour <annema.dufour@gmail.com>
2023-06-13 12:17:02 +0200

    Chapter 15: Complete gallery of shapes exercise

--- chapter_15/15.2-Basic_shapes/end/js/main.js
+++ chapter_15/15.2-Basic_shapes/end/js/main.js
@@ -9,17 +9,13 @@ const devicePixelRatio = window.devicePixelRatio;
 console.log("devicePixelRatio", devicePixelRatio);
 
 // Set the width and height of the canvas element
-const setCanvasSize = () => {
-  const width = container.offsetWidth;
-  const height = 0.333 * width;
-  console.log("width:", width, "height:", height);
+const width = container.offsetWidth;
+const height = 0.333 * width;
+console.log("width:", width, "height:", height);
 
-  canvas
-    .attr("width", width * devicePixelRatio)
-    .attr("height", height * devicePixelRatio);
-};
-setCanvasSize();
-window.addEventListener("resize", setCanvasSize);
+canvas
+  .attr("width", width * devicePixelRatio)
+  .attr("height", height * devicePixelRatio);
 
 // The canvas context is an object with properties and methods 
 // that you can use to render graphics inside the canvas element. 
@@ -28,50 +24,46 @@ const context = canvas.node().getContext("2d");
 context.scale(devicePixelRatio, devicePixelRatio);
 
 // Drawing the line
-context.strokeStyle = "#000";
-context.lineWidth = 1;
 context.beginPath();
-context.moveTo(66.67, 60);
-context.lineTo(186.67, 300);
+context.moveTo(66, 60);
+context.lineTo(186, 300);
 context.stroke();
 
-// // Drawing the rectangles
-// context.fillStyle = "#6ba5d7";
-// context.fillRect(260, 25, 120, 60); // x, y, width, height
+// Drawing the rectangles
+context.rect(346, 33, 160, 80); // x, y, width, height
+context.roundRect(346, 133, 160, 80, [20]); // x, y, width, height, [radii]
+context.fillStyle = "#6ba5d7";
+context.fill();
 
-// context.beginPath();
-// context.roundRect(260, 100, 120, 60, [20]);
-// context.fill();
+context.beginPath();  // Reset
+context.rect(346, 233, 106, 80);
+context.strokeStyle = "#6ba5d7";
+context.stroke();
 
-// context.fillStyle = "#fff";
-// context.fillRect(260, 175, 60, 60);
-// context.strokeStyle = "#6ba5d7";
-// context.strokeRect(260, 175, 60, 60);
+// Drawing the circles
+context.beginPath();  // Try without it to see how the latest rectangle get connected to the first circle
+context.arc(706, 106, 66, 0,  2 * Math.PI);  // x, y, radius, startAngle, endAngle
+context.strokeStyle = "#81c21c";
+context.lineWidth = 3;
+context.stroke();
 
-// // Drawing the circles
-// context.strokeStyle = "#81c21c";
-// context.lineWidth = 3;
-// context.beginPath();  // Try without it to see how another line is drawn
-// context.arc(530, 80, 50, 0,  2 * Math.PI, true);  // x, y, radius, startAngle, endAngle, isCounterClockwise
-// context.stroke();
+context.beginPath();
+context.ellipse(706, 273, 66, 40, 0, 0, 2 * Math.PI);  // x, y, radiusX, radiusY, startAngle, endAngle
+context.fillStyle = "#81c21c";
+context.fill();
 
-// context.fillStyle = "#81c21c";
-// context.beginPath();
-// context.ellipse(530, 205, 50, 30, 0, 0, 2 * Math.PI);
-// context.fill();
+// Drawing the path
+const path = new Path2D("M900 200 C 945 110, 965 110, 1010 200 S 1075 293, 1120 200");
+context.strokeStyle = "#773b9a";
+context.stroke(path);
 
-// // Drawing the path
-// context.strokeStyle = "#773b9a";
-// const path = new Path2D("M680 150 C 710 80, 725 80, 755 150 S 810 220, 840 150");
-// context.stroke(path);
+// Adding the text elements
+context.fillStyle = "#636466";
+context.font = "16px monospace";
+context.fillText("line", 80, 346);
+context.fillText("rect", 346, 346);
+context.fillText("path", 973, 346);
 
-// // Adding the text elements
-// context.fillStyle = "#636466";
-// context.font = "16px monospace";
-// context.fillText("line", 60, 260);
-// context.fillText("rect", 260, 260);
-// context.fillText("path", 730, 260);
-
-// context.textAlign = "center";
-// context.fillText("circle", 530, 155);
-// context.fillText("ellipse", 530, 260);
+context.textAlign = "center";
+context.fillText("circle", 706, 206);
+context.fillText("ellipse", 706, 346);
